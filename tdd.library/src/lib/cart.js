109c17183f55a95cd468e6e7b1437b1b94a85b01
Hoffmano <trotogamer@gmail.com>
2021-02-15 16:19:37 -0300

    feat(cart): permit to checkout cart

--- tdd.library/src/lib/cart.js
+++ tdd.library/src/lib/cart.js
@@ -1,12 +1,21 @@
 import { gt, equals } from 'ramda'
+import Dinero from 'dinero.js'
+const Money = Dinero
+
+Money.defaultCurrency = 'BRL'
+Money.defaultPrecision = 2
+Money.defaultAmount = 0
 
 const getTotal = cart =>
   cart.reduce(
-    (accumulator, item) => accumulator + item.quantity * item.product.price,
-    0,
+    (accumulator, item) =>
+      accumulator.add(Money({ amount: item.quantity * item.product.price })),
+    Money(),
   )
 
-const addItem = (cart, item) => {
+const addItem = (cart, item, quantity = 1) => {
+  if (quantity) item = { ...item, quantity: quantity }
+
   const foundItemIndex = cart.findIndex(cartItem =>
     equals(cartItem.product, item.product),
   )
@@ -20,4 +29,35 @@ const addItem = (cart, item) => {
   return cart
 }
 
-export { getTotal, addItem }
+const updateQuantity = (cart, item, quantity) => {
+  item = { ...item, quantity }
+
+  const foundItemIndex = cart.findIndex(cartItem =>
+    equals(cartItem.product, item.product),
+  )
+
+  if (gt(foundItemIndex, -1)) {
+    cart[foundItemIndex].quantity += item.quantity
+
+    if (cart[foundItemIndex].quantity <= 0) {
+      cart.splice(foundItemIndex, 1)
+    }
+  } else {
+    cart.push(item)
+  }
+
+  return cart
+}
+
+const summary = cart => {
+  const total = getTotal(cart).getAmount()
+  const checkout = { items: cart, total }
+  return checkout
+}
+
+const checkout = cart => {
+  const checkout = summary(cart)
+  return { updatedCart: [], checkoutCart: checkout }
+}
+
+export { getTotal, addItem, updateQuantity, summary, checkout }
--- tdd.library/src/tests/unit/cart.spec.js
+++ tdd.library/src/tests/unit/cart.spec.js
@@ -1,72 +1,91 @@
-import { getTotal, addItem } from '../../lib/cart'
+import { getTotal, updateQuantity, checkout, summary } from '../../lib/cart'
+
+let sneakers = {
+  product: {
+    title: 'Tênis',
+    price: 5000,
+  },
+  quantity: 1,
+}
+
+let shirt = {
+  product: {
+    title: 'Shirt',
+    price: 3000,
+  },
+  quantity: 1,
+}
 
 describe('CART', () => {
   it('should return 0 when getTotal() is called in a newly created cart', () => {
-    const cart = []
+    let cart = []
 
-    expect(getTotal(cart)).toEqual(0)
+    expect(getTotal(cart).getAmount()).toEqual(0)
   })
 
   it('should add an item to cart', () => {
     let cart = []
 
-    const item = {
-      product: {
-        title: 'Tenis',
-        price: 5000,
-      },
-      quantity: 2,
-    }
+    cart = updateQuantity(cart, sneakers, 1)
 
-    cart = addItem(cart, item)
-
-    expect(cart).toEqual([
-      {
-        product: {
-          title: 'Tenis',
-          price: 5000,
-        },
-        quantity: 2,
-      },
-    ])
+    expect(cart).toEqual([sneakers])
   })
 
   it('should allow just one item of each product in the cart', () => {
     let cart = []
 
-    const item = {
-      product: {
-        title: 'Tenis',
-        price: 5000,
-      },
-      quantity: 2,
-    }
+    cart = updateQuantity(cart, sneakers, 1)
+    cart = updateQuantity(cart, sneakers, 2)
 
-    cart = addItem(cart, item)
-    cart = addItem(cart, {...item, quantity: 1})
-
-    expect(cart).toEqual([
-      {
-        product: {
-          title: 'Tenis',
-          price: 5000,
-        },
-        quantity: 3,
-      },
-    ])
+    expect(cart).toEqual([{ ...sneakers, quantity: 3 }])
   })
 
   it('should multiply quantity by price and return the total amount', () => {
-    const cart = [
-      {
-        product: {
-          title: 'Tenis',
-          price: 5000,
-        },
-        quantity: 2,
-      },
-    ]
+    let cart = []
 
-    expect(getTotal(cart)).toEqual(10000)
+    cart = updateQuantity(cart, sneakers, 2)
+
+    expect(getTotal(cart).getAmount()).toEqual(10000)
+  })
+
+  it('should update total amount in cart', () => {
+    let cart = []
+
+    expect(getTotal(cart).getAmount()).toEqual(0)
+
+    cart = updateQuantity(cart, shirt, 1)
+    expect(getTotal(cart).getAmount()).toEqual(3000)
+
+    cart = updateQuantity(cart, sneakers, 1)
+    expect(getTotal(cart).getAmount()).toEqual(8000)
+  })
+
+  it('should remove some item from cart', () => {
+    let cart = []
+
+    cart = updateQuantity(cart, shirt, 1)
+    cart = updateQuantity(cart, shirt, -1)
+    expect(cart).toEqual([])
+  })
+
+  it('should remove some item from cart', () => {
+    let cart = []
+
+    cart = updateQuantity(cart, shirt, 1)
+    cart = updateQuantity(cart, sneakers, 11)
+
+    expect(summary(cart)).toMatchSnapshot()
+  })
+
+  it('should cleanup cart', () => {
+    let cart = []
+
+    cart = updateQuantity(cart, shirt, 1)
+    cart = updateQuantity(cart, sneakers, 11)
+
+    const { updatedCart, checkoutCart } = checkout(cart)
+    
+    expect(getTotal(updatedCart).getAmount()).toEqual(0)
+    expect(checkoutCart).toMatchSnapshot()
   })
 })
